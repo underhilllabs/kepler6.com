81cac0e0fedeb10c9661fea1827c0c8a6e2d12b5
Gray Jay <grayjay@denvertech.org>
2012-06-30 21:57:13 -0400

    updated the kepler6_omega_subtheme
    added a screenshot, added a gradient to header and menu.
    added login/logout to the navigation menu.

--- drupal-7.14/sites/all/themes/kepler6_omega_subtheme/templates/html.tpl.php
+++ drupal-7.14/sites/all/themes/kepler6_omega_subtheme/templates/html.tpl.php
@@ -2,7 +2,7 @@
 <html lang="<?php print $language->language; ?>" dir="<?php print $language->dir; ?>"<?php print $rdf->version . $rdf->namespaces; ?>>
 <head<?php print $rdf->profile; ?>>
   <?php print $head; ?>
-  <title><?php print $head_title; ?></title>  
+  <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
   <!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
@@ -13,6 +13,7 @@
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
+  <!-- <div class="starburst headerstar"><span><span><span></span></span></span></div> -->
   <?php print $page_top; ?>
   <?php print $page; ?>
   <?php print $page_bottom; ?>
